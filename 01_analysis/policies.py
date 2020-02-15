c2d4a3adb063ba5f7185d020465cf512d434ad55
Brendan Cooley <brendan.cooley@gmail.com>
2020-02-14 19:47:46 -0500

    attempting with fixed c_hat

--- 01_analysis/policies.py
+++ 01_analysis/policies.py
@@ -447,8 +447,12 @@ class policies:
         if nash_eq == False:
             x_L[self.x_len+self.lambda_i_len*self.N:self.x_len+self.lambda_i_len*self.N+self.N] = 1 # vs
             x_U[self.x_len+self.lambda_i_len*self.N:self.x_len+self.lambda_i_len*self.N+self.N] = np.max(self.ecmy.tau) # vs
-            x_L[self.x_len+self.lambda_i_len*self.N+self.N] = 0  # c_hat
+            # x_L[self.x_len+self.lambda_i_len*self.N+self.N] = 0  # c_hat
+            x_L[self.x_len+self.lambda_i_len*self.N+self.N+2] = .25
+            x_U[self.x_len+self.lambda_i_len*self.N+self.N+2] = .25  # fix c_hat
             x_L[self.x_len+self.lambda_i_len*self.N+self.N+1] = 0  # alpha
+            # x_L[self.x_len+self.lambda_i_len*self.N+self.N+2] = 1
+            # x_U[self.x_len+self.lambda_i_len*self.N+self.N+2] = 1  # fix gamma at 1
             # x_L[self.x_len+self.lambda_i_len*self.N+self.N+2] = 1
             # x_U[self.x_len+self.lambda_i_len*self.N+self.N+2] = 1  # fix gamma at 1
             x_L[self.x_len+self.lambda_i_len*self.N+self.N+2] = 0
