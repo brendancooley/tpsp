33b1d2373d6a948fc777b2946cc460e683f2f4ec
bcooley <brendan.cooley@gmail.com>
2020-02-26 16:32:04 -0500

    drop extraneous stuff policies

--- 01_analysis/testBay.py
+++ 01_analysis/testBay.py
@@ -72,46 +72,43 @@ E = Eq + Ex
 
 data = {"tau":tau,"Xcif":Xcif,"Y":Y,"E":E,"r":r,"D":D,"W":W,"M":M, "ccodes":ccodes}  # Note: log distance
 
-theta_dict_1 = dict()
-theta_dict_1["c_hat"] = .25
-theta_dict_1["alpha0"] = 0
-theta_dict_1["alpha1"] = .001
-theta_dict_1["gamma"] = .72
-
-theta_dict_2 = dict()
-theta_dict_2["c_hat"] = .78
-theta_dict_2["alpha0"] = -5
-theta_dict_2["alpha1"] = -.0001
-theta_dict_2["gamma"] = 5
+theta_dict = dict()
+theta_dict["c_hat"] = .25
+theta_dict["alpha0"] = 0
+theta_dict["alpha1"] = .001
+theta_dict["gamma"] = .72
 
 # TODO try just running inner loop, problem is that values of v change with theta as well, no reason we should run theta until covergence rather than iterating on v first.
 
 imp.reload(policies)
 pecmy = policies.policies(data, params, ROWname, results_path=resultsPath)  # generate pecmy and rcv vals
 
-test = pecmy.alpha1_min(.01)
-
 v_test = np.array([1.10, 1.37, 1.96, 1.11, 1.00, 1.19])
-wv = pecmy.war_vals(v_test, pecmy.m, theta_dict_1)
+wv = pecmy.war_vals(v_test, pecmy.m, theta_dict)
 np.diag(wv)
 # wv = np.clip(wv, 0, np.inf)
-id = 5
+id = 0
 # np.fill_diagonal(wv, 0)
 
+# x_sv = pecmy.v_sv(0, np.ones(pecmy.x_len), v_test)
+#
+# x = pecmy.br_ipyopt(x_sv, v_test, id, wv[:,id])
+# ge_dict = pecmy.ecmy.rewrap_ge_dict(x)
+# print(ge_dict["tau_hat"]*pecmy.ecmy.tau)
+
 x_lbda, obj, status = pecmy.Lsolve_i_ipopt(id, v_test, wv[:,id])
-np.savetxt("01_analysis/x.csv", x_lbda, delimiter=",")
 
-x_lbda = np.genfromtxt("x.csv", delimiter=",")
-ge_x = x_lbda[0:pecmy.x_len]
-ge_dict1 = pecmy.ecmy.rewrap_ge_dict(ge_x)
-ge_dict1["tau_hat"] * pecmy.ecmy.tau
-wv[:,id]
-
-pecmy.ecmy.geq_solve(ge_dict1["tau_hat"], np.ones(pecmy.N))
-
-pecmy.G_hat(ge_x, v_test, id, all=True)
-cons_test = pecmy.Lzeros_i_cons(x_lbda, np.zeros(pecmy.hhat_len + (pecmy.hhat_len + pecmy.N - 1) + pecmy.N + pecmy.N), id, v_test, wv[:,id])
-cons_test[pecmy.hhat_len:]
-cons_test[0:pecmy.hhat_len]
-
-# x, obj, status = pecmy.estimator(v_test, pecmy.unwrap_theta(theta_dict_1), pecmy.mzeros, nash_eq=True)
+# np.savetxt("01_analysis/x.csv", x_lbda, delimiter=",")
+#
+# x_lbda = np.genfromtxt("x.csv", delimiter=",")
+# ge_x = x_lbda[0:pecmy.x_len]
+# ge_dict1 = pecmy.ecmy.rewrap_ge_dict(ge_x)
+# ge_dict1["tau_hat"] * pecmy.ecmy.tau
+# wv[:,id]
+#
+# pecmy.ecmy.geq_solve(ge_dict1["tau_hat"], np.ones(pecmy.N))
+#
+# pecmy.G_hat(ge_x, v_test, id, all=True)
+# cons_test = pecmy.Lzeros_i_cons(x_lbda, np.zeros(pecmy.hhat_len + (pecmy.hhat_len + pecmy.N - 1) + pecmy.N + pecmy.N), id, v_test, wv[:,id])
+# cons_test[pecmy.hhat_len:]
+# cons_test[0:pecmy.hhat_len]
