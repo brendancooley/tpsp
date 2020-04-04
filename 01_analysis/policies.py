40ee3293096d06cd492aef3c09d050010e8853ea
bcooley <brendan.cooley@gmail.com>
2020-04-04 16:03:09 -0400

    reinstate tau lb and reattempt est

--- 01_analysis/policies.py
+++ 01_analysis/policies.py
@@ -1055,8 +1055,8 @@ class policies:
     def geq_lb(self):
 
         lb_dict = dict()
-        lb_dict["tau_hat"] = np.reshape(np.repeat(0, self.N**2), (self.N, self.N))
-        # lb_dict["tau_hat"] = 1 / self.ecmy.tau
+        # lb_dict["tau_hat"] = np.reshape(np.repeat(0, self.N**2), (self.N, self.N))
+        lb_dict["tau_hat"] = 1 / self.ecmy.tau
         # lb_dict["tau_hat"] = 1.01 / self.ecmy.tau
         np.fill_diagonal(lb_dict["tau_hat"], 1)
         lb_dict["D_hat"] = np.repeat(1, self.N)
--- 01_analysis/testBay.py
+++ 01_analysis/testBay.py
@@ -21,9 +21,9 @@ projectPath = basePath + "/Github/tpsp/"
 projectFiles = basePath + "/Dropbox (Princeton)/1_Papers/tpsp/01_data/"
 
 
-size = "mini/"
+size = "mid/"
 sv_fname = "out/mini_sv.csv"
-out_fname = "out/mini_eq_test.csv"
+out_fname = "out/mid_est5.csv"
 # sv = np.genfromtxt(sv_fname, delimiter=',')
 
 helpersPath = os.path.expanduser(projectPath + "source/")
@@ -125,7 +125,7 @@ pecmy.peace_probs(np.ones(pecmy.x_len), ft_id[-pecmy.hhat_len:], id, pecmy.m, v,
 
 # print(sv)
 # x, obj, status = pecmy.estimator(v, theta_x, pecmy.m, sv=sv, nash_eq=False)
-x, obj, status = pecmy.estimator(v, theta_x, pecmy.m, sv=None, nash_eq=True)
+x, obj, status = pecmy.estimator(v, theta_x, pecmy.m, sv=None, nash_eq=False)
 
 x_dict = pecmy.rewrap_xlhvt(x)
 ge_dict = pecmy.ecmy.rewrap_ge_dict(x_dict["ge_x"])
