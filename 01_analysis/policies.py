e9145b6651a1ff97c7afbda0800880da2cd064c7
bcooley <brendan.cooley@gmail.com>
2020-04-05 19:22:23 -0400

    reset cost bounds

--- 01_analysis/policies.py
+++ 01_analysis/policies.py
@@ -1184,8 +1184,8 @@ class policies:
             x_U[b] = a_ub # alpha1 upper
             b += 1
             # x_L[b:b+self.N] = .6  # cs
-            x_L[b:b+self.N] = opt.root(self.pp_wrap_C, .5, args=(.15, ))['x']  # cs
-            x_U[b:b+self.N] = 10
+            x_L[b:b+self.N] = opt.root(self.pp_wrap_C, .5, args=(.25, ))['x']  # cs
+            x_U[b:b+self.N] = 5
             # x_U[b] = self.alpha1_ub  # alpha1 upper
             # x_L[b] = -np.inf  # alpha1 lower
             # x_U[b] = np.inf  # alpha1 upper
