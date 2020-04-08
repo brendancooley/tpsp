9d07de9aeacf5f0319d6b5d97ca509e6f8d26cb7
bcooley <brendan.cooley@gmail.com>
2020-04-08 19:07:49 -0400

    Chat bounds

--- 01_code/testBay.py
+++ 01_code/testBay.py
@@ -23,7 +23,7 @@ projectFiles = basePath + "/Dropbox (Princeton)/1_Papers/tpsp/01_data/"
 
 size = "mid/"
 sv_fname = "out/mini_sv.csv"
-out_fname = "out/mid_est_test12.csv"
+out_fname = "out/mid_est_test13.csv"
 # sv = np.genfromtxt(sv_fname, delimiter=',')
 
 helpersPath = os.path.expanduser(projectPath + "source/")
