b2dc047c2eccb97bdbcb40bc1ca93b6e99429c27
Brendan Cooley <brendan.cooley@gmail.com>
2020-04-22 16:05:48 -0400

    Update bootstrap.py

--- 01_code/bootstrap.py
+++ 01_code/bootstrap.py
@@ -1,4 +1,6 @@
 import sys
+import threading
+import logging
 
 import results
 
@@ -8,8 +10,20 @@ location = "local"
 size = "mid/"
 
 results_base = True
+M = 100 # number of bootstrap iterations
 
 if results_base == True:
-    r_base = results(location, size)
+    print("results base...")
+    r_base = results.results(location, size)
     r_base.compute_estimates()
     r_base.unravel_estimates()
+
+def bootstrap_i(id):
+    logging.info("Thread starting " + str(id))
+    r_id = results.results(location, size, True, id)
+    r_id.compute_estimates()
+    logging.info("Thread finishing " + str(id))
+
+if __name__ == '__main__':
+
+    threads = list()
