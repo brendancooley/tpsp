ba328cf34f7ea93b10e7c77602f8fb78acdedef5
Brendan Cooley <brendan.cooley@gmail.com>
2020-04-25 13:04:22 -0400

    Update policies.py

--- 01_code/policies.py
+++ 01_code/policies.py
@@ -1164,14 +1164,14 @@ class policies:
         theta_dict_lb["eta"] = 1
         theta_dict_lb["gamma"] = -.5
         theta_dict_lb["c_hat"] = c_lb
-        theta_dict_lb["alpha1"] = -1.  # distance coefficient
+        theta_dict_lb["alpha1"] = -.75  # distance coefficient
         theta_dict_lb["alpha2"] = -.5  # gdp coefficient
         theta_dict_lb["C"] = np.repeat(c_lb, self.N)
         lb = self.unwrap_theta(theta_dict_lb)
 
         theta_dict_ub = dict()
         theta_dict_ub["eta"] = 1
-        theta_dict_ub["gamma"] = 2.
+        theta_dict_ub["gamma"] = 1.75
         theta_dict_ub["c_hat"] = c_ub
         theta_dict_ub["alpha1"] = 1. # distance coefficient
         theta_dict_ub["alpha2"] = 1.  # gdp coefficient
